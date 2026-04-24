dd8d2b1cc2e9445d241396366cd30609376253a6
David <davidsvnchz@gmail.com>
2026-04-24 03:00:11 +0200

    Header añadido a firebase y modularizado en todas las partes del proyecto

---
+++ HEADER/JS-Header/firebaseheader.js
@@ -0,0 +1,43 @@
+import { db } from "../../JS-Firebase/firebase-init.js";
+import {
+  collection,
+  getDocs, doc, getDoc
+} from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+
+
+async function cargarHeader() {
+    try {
+        const refTextos = doc(db, "header", "textos");
+        const snapTextos = await getDoc(refTextos);
+
+        if (!snapTextos.exists()) {
+            console.error("❌ No existe header/textos");
+            return;
+        }
+
+        const data = snapTextos.data();
+
+        // CATEGORIAS
+        document.getElementById("HeaderContacto").textContent = data.HeaderContacto;
+        document.getElementById("HeaderInicio").textContent = data.HeaderInicio;
+        document.getElementById("HeaderProductos").textContent = data.HeaderProductos;
+        document.getElementById("HeaderProductosLatinos").textContent = data.HeaderProductosLatinos;
+
+        // TITULOS
+        document.getElementById("TituloHeader").textContent = data.TituloHeader;
+        document.getElementById("SubtituloHeader").textContent = data.SubtituloHeader;
+        
+        
+        //EMOJIS
+        document.getElementById("Headeremoji_trofeo").textContent = data.Slogan;
+        document.getElementById("emoji_cesta").textContent = data.Frase_destacada;
+
+     
+
+        console.log("✔ Textos cargados correctamente");
+
+    } catch (error) {
+        console.error("❌ Error cargando textos:", error);
+    }
+}
+cargarHeader();
